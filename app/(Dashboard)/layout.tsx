bd58188ca54ea65917f4a210c45d157294b93525
haseeb-sheikh10 <haseeb_irfan1368@hotmail.com>
2024-06-28 20:30:15 +0500

    bug fix

--- app/(Dashboard)/layout.tsx
+++ app/(Dashboard)/layout.tsx
@@ -2,23 +2,32 @@ import React from "react";
 import Navbar from "./_components/Navbar";
 import MainSidebar from "./_components/main-sidebar/MainSidebar";
 import OrgSidebar from "./_components/org-sidebar/OrgSidebar";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { RedirectToSignIn } from "@clerk/clerk-react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="h-full flex">
-      <MainSidebar />
-      <section className="h-full w-full">
-        <div className="flex h-full">
-          <OrgSidebar />
-          <div className="flex-1 h-screen">
-            <Navbar />
-            <div className="p-6 h-[calc(100%-60px)] bg-primary-foreground overflow-y-auto">
-              {children}
+    <>
+      <Unauthenticated>
+        <RedirectToSignIn />
+      </Unauthenticated>
+      <Authenticated>
+        <main className="h-full flex">
+          <MainSidebar />
+          <section className="h-full w-full">
+            <div className="flex h-full">
+              <OrgSidebar />
+              <div className="flex-1 h-screen">
+                <Navbar />
+                <div className="p-6 h-[calc(100%-60px)] bg-primary-foreground overflow-y-auto">
+                  {children}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
-    </main>
+          </section>
+        </main>
+      </Authenticated>
+    </>
   );
 };
 
